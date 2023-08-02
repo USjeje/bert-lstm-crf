9bfbcf58cd77feab0ac0c2a9d93c657cc04cd1ee
USjeje <34546354+USjeje@users.noreply.github.com>
2023-08-02 19:19:07 +0800

    Delete shell directory

--- shell/deploy.sh
+++
@@ -1,3 +0,0 @@
-nohup sh shell/start-server.sh > log/server.log 2>&1 & echo $! > log/server-pidfile
-echo '[SERVER] NER server start, please see app/server.log for details.'
-while true; do sleep 1; done
